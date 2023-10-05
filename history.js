f80ce9d03e447dcd6aa41225b58c1b342b7ddfc6
Sanchana Arun <130022933+sanchana-arun@users.noreply.github.com>
2023-10-06 04:13:24 +0530

    Update history.js

--- history.js
+++ history.js
@@ -1,6 +1,5 @@
-// Wait for the DOM to fully load
 document.addEventListener("DOMContentLoaded", function () {
-  // Add animation to timeline items on scroll
+
   function animateTimelineItems() {
     const timelineItems = document.querySelectorAll('.timeline-item');
 
